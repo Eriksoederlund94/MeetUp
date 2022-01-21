44c6c4827a36711dde09f7e31edde196a5ac5983
Ace1313 <alex.cadjo@hotmail.com>
2022-01-21 09:34:04 +0100

    prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@
 import NavBar from './components/NavBar';
 
 function App() {
-  return (
-    <div className='App'>
-      <header>
-        <NavBar />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <NavBar />
+            </header>
+        </div>
+    );
 }
 
 export default App;
