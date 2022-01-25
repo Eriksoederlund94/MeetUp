853dcad542e6d932934fa5981dbe4e39f179c184
Ace1313 <alex.cadjo@hotmail.com>
2022-01-25 10:43:43 +0100

    Tests for comment section

--- src/feature/event/__test__/EventComment.test.tsx
+++ src/feature/event/__test__/EventComment.test.tsx
@@ -1,30 +1,15 @@
-import { fireEvent, getNodeText, render, screen, waitFor, within } from '@testing-library/react';
+import {
+    findByPlaceholderText,
+    getByPlaceholderText,
+    render,
+    screen,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import EventComment from '../../event/EventComment';
 import EventItemCard from '../../event/EventItemCard';
 import { mockEvents } from '../../../mockEvents';
-import { useState } from 'react';
-
-interface CommentContainerProps {
-    showComments: boolean;
-}
-
-function CommentContainer({ showComments }: CommentContainerProps) {
-    const [show, setShow] = useState(false);
-    return <div data-testid='event-item-container' className='event-card-item'>
-        <button onClick={() => setShow(!show)}>
-            {show ? 'Hide comments' : 'Show comments'}
-        </button>
-        {showComments &&
-            <div data-testid='comment-container'>
-                <h4>Comments</h4>
-            </div>
-        }
-    </div>
-}
 
 describe('Event comment', () => {
-
     it('Should render without crashing', () => {
         render(<EventComment />);
     });
@@ -32,41 +17,59 @@ describe('Event comment', () => {
     it('Should display comment in comment section', () => {
         render(<EventItemCard {...mockEvents[0]} />);
 
-        const buttonElement = screen.getByRole('button', { name: /Show comments/i });
-    
+        const buttonElement = screen.getByRole('button', {
+            name: /Show comments/i,
+        });
+
         userEvent.click(buttonElement);
 
         expect(buttonElement).toHaveTextContent('Hide comments');
     });
 
-    /* it('Should set showComments to true', () => {
-        render(<EventItemCard {...mockEvents[0]} />);
-        const buttonElement = screen.getByRole('button', { name: /Hide comments/i });
-        userEvent.click(buttonElement);
-    }); */
-
-    /* it('Should show input field for new comment', async () => {
+    it('Should show input field for new comment', () => {
         render(<EventItemCard {...mockEvents[0]} />);
 
-        const buttonElement = screen.getByRole('button', { name: /Show comments/i });
-        const commentContainer = await screen.findByTestId('comment-container');
-       
+        const buttonElement = screen.getByRole('button', {
+            name: /Show comments/i,
+        });
         userEvent.click(buttonElement);
-        
-        await waitFor(() => expect(commentContainer).toBeInTheDocument());
-    }); */
-    
+        const commentContainer = screen.getByTestId('comment-container');
+        expect(commentContainer).toBeInTheDocument();
+    });
+    it('Should add a new comment', () => {
+        render(<EventItemCard {...mockEvents[0]} />);
 
-    it('Should show input field for new comment', async () => {
-        render(<CommentContainer showComments={false} />);
-        //const buttonElement = screen.getByRole('button', { name: /Show comments/i });
-        const container = await screen.getByTestId('event-item-container');
-        const commentContainer = within(container).getByText('Comments');
-        const showCommentsBtn = screen.getByRole('button');
+        const buttonEle = screen.getByRole('button', {
+            name: /Show comments/i,
+        });
 
-        //fireEvent.click(showCommentsBtn);
-       
-        userEvent.click(showCommentsBtn);
-        await waitFor(() => expect(commentContainer).toBeInTheDocument());
+        userEvent.click(buttonEle);
+
+        const buttonElement = screen.getByRole('button', {
+            name: /add comment/i,
+        });
+        userEvent.click(buttonElement);
+
+        const inputElement = screen.getByPlaceholderText('Add comment');
+
+        expect(inputElement).toBeInTheDocument();
+    });
+
+    it('Should add a comment into the input field', () => {
+        render(<EventItemCard {...mockEvents[1]} />);
+        const buttonEle = screen.getByRole('button', {
+            name: /Show comments/i,
+        });
+
+        userEvent.click(buttonEle);
+
+        const buttonElement = screen.getByRole('button', {
+            name: /add comment/i,
+        });
+        userEvent.click(buttonElement);
+
+        const inputElement = screen.getByText('Hello there!');
+
+        expect(inputElement).toBeInTheDocument();
     });
 });
